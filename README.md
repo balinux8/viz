13672fabc2ba2bd8ad530383bc8c50ed2f7108fa
balinux8 <balinux8@gmail.com>
2022-09-01 22:36:15 +0700

    crawls graphviz colors

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# viz
+# viz is a set of tools and libraries to visualize things
+
+```
+Learning things is hard, learn without visuallization is even harder!
+```
--- internal/crawlgviz/crawlgviz.go
+++ internal/crawlgviz/crawlgviz.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/antchfx/htmlquery"
+	"golang.org/x/net/html"
 	"net/http"
+	"regexp"
 )
 
+var re = regexp.MustCompile("^\\s*([a-z1-9]*)\\s*color scheme\\s*")
+
+type Color struct {
+	scheme   string // x11, svg
+	group    string // x11, svg, blue9, blue3
+	hexColor string // #ff0011
+	name     string // lightblue, 1, 2
+}
+
 func main() {
+	var colorschemes []Color
+
 	resp, err := http.Get("https://graphviz.org/doc/info/colors.html")
 	if err != nil {
 		panic(err)
@@ -25,19 +38,93 @@ func main() {
 	}
 
 	for _, c := range colorSchemes {
-		fmt.Println(htmlquery.SelectAttr(c, "id"))
-		node := c.NextSibling
-		for node != nil {
-			if htmlquery.SelectAttr(c.NextSibling, "class") == "gv-colors" {
-				gvColors := c.NextSibling
-				colors, err := htmlquery.QueryAll(gvColors, "//td")
-				if err != nil {
-					panic(err)
+		id := func(node *html.Node) string { return htmlquery.SelectAttr(node, "id") }
+		class := func(node *html.Node) string { return htmlquery.SelectAttr(node, "class") }
+		innerText := func(node *html.Node) string { return htmlquery.InnerText(node) }
+		title := func(node *html.Node) string { return htmlquery.SelectAttr(node, "title") }
+
+		node := c
+		switch scheme := id(node); scheme {
+		case "x11":
+			{
+				for node = node.NextSibling; node != nil; node = node.NextSibling {
+					if class(node) == "gv-colors" {
+						colors, err := htmlquery.QueryAll(node, "//td")
+						if err != nil {
+							panic(err)
+						}
+						for _, colorNode := range colors {
+							name := innerText(colorNode)
+							hexColor := title(colorNode)
+							colorschemes = append(colorschemes, Color{
+								scheme:   scheme,
+								group:    scheme,
+								hexColor: hexColor,
+								name:     name,
+							})
+						}
+
+						break
+					}
 				}
-				for _, node := range colors {
-					fmt.Println(htmlquery.InnerText(node))
+			}
+		case "svg":
+			{
+				for node = node.NextSibling; node != nil; node = node.NextSibling {
+					if class(node) == "gv-colors" {
+						colors, err := htmlquery.QueryAll(node, "//td")
+						if err != nil {
+							panic(err)
+						}
+						for _, colorNode := range colors {
+							name := innerText(colorNode)
+							hexColor := title(colorNode)
+							colorschemes = append(colorschemes, Color{
+								scheme:   scheme,
+								group:    scheme,
+								hexColor: hexColor,
+								name:     name,
+							})
+						}
+
+						break
+					}
+				}
+			}
+		case "brewer":
+			for node = node.NextSibling; node != nil; node = node.NextSibling {
+				if node.Type == html.TextNode {
+					text := node.Data
+					// text must be in form like: \n\nylorrd5 color scheme
+					list := re.FindStringSubmatch(text)
+					var group string
+					if len(list) >= 2 {
+						group = list[1]
+						// find colors
+						for node = node.NextSibling; node != nil; node = node.NextSibling {
+							if class(node) == "gv-colors" {
+								colors, err := htmlquery.QueryAll(node, "//td")
+								if err != nil {
+									panic(err)
+								}
+								for _, colorNode := range colors {
+									name := innerText(colorNode)
+									hexColor := title(colorNode)
+									colorschemes = append(colorschemes, Color{
+										scheme:   scheme,
+										group:    group,
+										hexColor: hexColor,
+										name:     name,
+									})
+								}
+								break
+							}
+						}
+					}
 				}
 			}
 		}
 	}
+
+	fmt.Println(colorschemes)
 }
